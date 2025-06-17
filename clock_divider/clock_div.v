036a91f6a7980db418641b94da8bbbe6acf076b2
Sidd168 <sid32263@gmail.com>
2025-06-18 00:03:30 +0530

    Update clock_div.v

--- clock_divider/clock_div.v
+++ clock_divider/clock_div.v
@@ -1,1 +1,24 @@
+module clock_div (
+    input wire clk,          
+    input wire rst,          
+    output reg divided_clk   
+);
+
+    reg [2:0] counter;       
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            counter <= 3'b000;
+            divided_clk <= 1'b0;
+        end else begin
+            if (counter == 3'b111) begin
+                counter <= 3'b000;
+                divided_clk <= ~divided_clk; // Toggle output clock
+            end else begin
+                counter <= counter + 1;
+            end
+        end
+    end
+
+endmodule
 
