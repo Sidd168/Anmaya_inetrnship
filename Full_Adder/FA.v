a227241a433fce98923b8c78d4ddc71ea2967818
Sidd168 <sid32263@gmail.com>
2025-06-19 14:44:20 +0530

    Update FA.v

--- Full_Adder/FA.v
+++ Full_Adder/FA.v
@@ -1,1 +1,10 @@
+module FA(input A, B, CIN, output SUM, COUT);
+    wire w1, w2, w3;
+
+    xor (w1, A, B);
+    xor (SUM, w1, CIN);
+    and (w2, w1, CIN);
+    and (w3, A, B);
+    or (COUT, w2, w3);
+endmodule
 
